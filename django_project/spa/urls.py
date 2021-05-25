9c4789f49b2fc450c66e1a0b487678034d3917f3
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-05-25 18:27:57 +0300

    page path : register, login, master_detail

--- django_project/spa/urls.py
+++ django_project/spa/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import homepage,services,masters
+from .views import *
 
 urlpatterns = [
-    path('',homepage),
+    path('',homepage,name='home'),
     path('services/',services),
     path('masters/',masters),
+    path('register/',register_page),
+    path('login/',login_page, name='login'),
+    path('master_detail/<int:master_id>/',master_detail,name='master_detail'),
+
 ]
