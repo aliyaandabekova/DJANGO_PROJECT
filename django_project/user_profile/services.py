221466e1904365db791313f8ceec4c1a5210a397
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-05-27 18:06:19 +0300

    create functions: for OrderCount, CountMoney

---
+++ django_project/user_profile/services.py
@@ -0,0 +1,16 @@
+from django.core.exceptions import ValidationError
+
+
+def incrementOrdetCount(profile):
+    profile.order_count += 1
+    profile.save()
+
+def countMoney(profile,instance):
+    price = instance.service.price
+    if instance.payment_method == 'wallet':
+        if profile.wallet >= price:
+            profile.wallet -= price
+            profile.save()
+            instance.status = 'closed'
+        else:
+            raise ValidationError('no money!')
