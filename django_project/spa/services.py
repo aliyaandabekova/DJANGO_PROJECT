45c7a5d2bee0f43c7af26901f6df8889ae68eeea
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-05-31 18:27:24 +0300

    add function for checking Expired of Certificates

--- django_project/spa/services.py
+++ django_project/spa/services.py
@@ -1,7 +1,16 @@
 from user_profile.models import Profile
+import datetime
 
 def profileCreate(profile_data,instance):
     name = profile_data.get('name')
     age = profile_data.get('age')
     email = profile_data.get('email')
     Profile.objects.create(name=name,age=age,email=email,user=instance)
+
+def checkExpiredCertificate(certificates):
+    for certificate in certificates:
+        cert_date = certificate.date_expired
+        today = datetime.date.today()
+        if today > cert_date:
+            certificate.status = 'dead'
+            certificate.save()
