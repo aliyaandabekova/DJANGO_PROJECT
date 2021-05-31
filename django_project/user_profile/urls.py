c11e753922ca3b60c6f73f2748c64c2ff2a1f23b
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-05-31 18:28:34 +0300

    add 2 urls for: My orders, Delete order

--- django_project/user_profile/urls.py
+++ django_project/user_profile/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('profile/',profile_page),
     path('order/<int:service_id>/',order_page,name='order'),
+    path('order/', my_orders, name='my_orders'),
+    path('delete_order/<int:order_id>/', delete_order),
 
 ]
