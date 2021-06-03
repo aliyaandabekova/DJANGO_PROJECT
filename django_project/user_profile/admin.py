391bbbc073ede8a3c4c9a3e75e57f2a606ba5a36
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-03 13:33:50 +0300

    change view of Orders,Profiles ( list -> table)

--- django_project/user_profile/admin.py
+++ django_project/user_profile/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register([Profile,Order])
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user','service','date_created','status_char','payment_method']
+admin.site.register(Order,OrderAdmin)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','photo','name','age','email','order_count','wallet']
+    readonly_fields = ['order_count','wallet']
+
+admin.site.register(Profile,ProfileAdmin)
