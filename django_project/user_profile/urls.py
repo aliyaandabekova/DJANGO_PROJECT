11b891a41c84b74e4f3e60319f08ec148c9dfcc5
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-03 13:39:17 +0300

    add url for update_order/

--- django_project/user_profile/urls.py
+++ django_project/user_profile/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('order/<int:service_id>/',order_page,name='order'),
     path('order/', my_orders, name='my_orders'),
     path('delete_order/<int:order_id>/', delete_order),
+    path('update_order/<int:order_id>/', update_order),
+
 
 ]
