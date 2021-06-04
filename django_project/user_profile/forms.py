493edb00f36d99b93524dd8a3f66e276e8d8bc08
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-04 17:28:46 +0300

    create form for Profile

--- django_project/user_profile/forms.py
+++ django_project/user_profile/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Order
+from .models import Order, Profile
 
 class OrderForm(ModelForm):
 
     class Meta:
         model = Order
         fields = ['user','service','payment_method']
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'photo', 'age', 'email']
