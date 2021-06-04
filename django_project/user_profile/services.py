ee8e6c29e5701f5c09bd315b9357a25589302e02
aliyaandabekova <andabekovaaliyaa@gmail.com>
2021-06-04 17:34:38 +0300

    add function count_sale and add condition in countMoney(True)

--- django_project/user_profile/services.py
+++ django_project/user_profile/services.py
@@ -3,6 +3,8 @@ from django.db import transaction
 from django.utils import timezone
 
 
+
+
 def time_check(order_date):
     date_now = timezone.now()
     delta = timezone.timedelta(minutes=5)
@@ -20,8 +22,16 @@ def countMoney(profile,instance):
     price = instance.service.price
     if instance.payment_method == 'wallet':
         if profile.wallet >= price:
+            if profile.sale_amount>0:
+                price = price - (price*profile.sale_amount)
             profile.wallet -= price
             profile.save()
             instance.status = 'closed'
         else:
             raise ValidationError('no money!')
+
+
+def count_sale(profile):
+    if profile.order_count>50:
+        profile.sale_amount = 0.05
+        profile.save()
